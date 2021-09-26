2fcf95313f6679865e4d536991153ca19985d6f6
shashinma <shashinma@icloud.com>
2021-09-26 05:18:51 +0300

    Upload files

---
+++ VoteApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VoteApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
