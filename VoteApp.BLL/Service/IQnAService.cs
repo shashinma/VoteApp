76ecf407b69aa2b3a9e4c84050ce7b46ae2a0b85
Mikhail Shashin <shashinma@icloud.com>
2023-07-06 15:59:44 +0300

    Create IQnAService.cs

---
+++ VoteApp.BLL/Service/IQnAService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using VoteApp.DAL;
+using VoteApp.ViewModels;
+
+namespace VoteApp.BLL.Service
+{
+	public interface IQnAService
+	{
+		PagedResult<QnAsViewModel> GetAll(int pageNumber, int pageSize);
+
+		Task<ExamViewModel> AddAsync(QnAsViewModel examVM);
+
+		IEnumerable<QnAs> GetAllExams();
+	}
+}
+
