fcc0a40cc391b87e05bd76f68096bc841e083e7a
Mikhail Shashin <shashinma@icloud.com>
2023-07-06 16:05:46 +0300

    Create QnAService.cs

---
+++ VoteApp.BLL/Service/QnAService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using VoteApp.ViewModels;
+
+namespace VoteApp.BLL.Service
+{
+	public class QnAService : IQnAService
+	{
+		public QnAService()
+		{
+		}
+
+        public Task<QnAsViewModel> AddAsync(QnAsViewModel QnAVM)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PagedResult<QnAsViewModel> GetAll(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<QnAsViewModel> GetAllExams()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsExamAttended(int examId, int studentId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
