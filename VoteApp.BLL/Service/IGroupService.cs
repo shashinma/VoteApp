0a1387fc3baef5e0a2dc283c5c5524cacc04491f
Mikhail Shashin <shashinma@icloud.com>
2023-07-06 14:51:18 +0300

    Update IGroupService.cs

--- VoteApp.BLL/Service/IGroupService.cs
+++ VoteApp.BLL/Service/IGroupService.cs
@@ -1,8 +1,18 @@
 ﻿using System;
+using VoteApp.DAL;
+using VoteApp.ViewModels;
+
 namespace VoteApp.BLL.Service
 {
 	public interface IGroupService
 	{
+		PagedResult<GroupViewModel> GetAllGroups(int pageNumber, int pageSize);
+
+		Task<GroupViewModel> AddGroupAsync(GroupViewModel groupVM);
+
+		IEnumerable<Groups> GetAllGroups();
+
+		GroupViewModel GetById(int groupId);
 	}
 }
 
