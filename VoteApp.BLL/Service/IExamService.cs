2485d55b60a40e3d415a7253fb1c8708f2cb1fe5
Mikhail Shashin <shashinma@icloud.com>
2023-07-06 15:28:07 +0300

    Update IExamService.cs

--- VoteApp.BLL/Service/IExamService.cs
+++ VoteApp.BLL/Service/IExamService.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using VoteApp.DAL;
+using VoteApp.ViewModels;
+
 namespace VoteApp.BLL.Service
 {
 	public interface IExamService
 	{
+		PagedResult<ExamViewModel> GetAll(int pageNumber, int pageSize);
 
+		Task<ExamViewModel> AddAsync(ExamViewModel examVM);
+
+		IEnumerable<Exams> GetAllExams();
 	}
 }
 
