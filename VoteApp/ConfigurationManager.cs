ee7bb267cb044efa22b4605b9b4e84528fafc3d2
Mikhail Shashin <shashinma@icloud.com>
2023-07-07 14:23:16 +0300

    Create ConfigurationManager.cs

---
+++ VoteApp/ConfigurationManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace VoteApp
+{
+    public class ConfigurationManager
+    {
+        private static IConfiguration configuration = null;
+
+        static ConfigurationManager()
+        {
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build();
+        }
+
+        public static string GetFilePath()
+        {
+            return configuration["CustomKey:BaseUrl"] + "file/";
+        }
+    }
+}
