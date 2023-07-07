308730b42e218010219d0e79d2725afaefaa7c39
Mikhail Shashin <shashinma@icloud.com>
2023-07-07 10:36:40 +0300

    Create SessionExtentions.cs

---
+++ VoteApp/SessionExtentions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.Json;
+
+namespace VoteApp
+{
+	public static class SessionExtentions
+	{
+		public static void Set<T>(this ISession session, string key, T value)
+		{
+			session.SetString(key, JsonSerializer.Serialize(value));
+		}
+
+		public static T Get<T>(this ISession session, string key)
+		{
+			var value = session.GetString(key);
+            return value == null ? default : JsonSerializer.Deserialize<T>(value);
+		}
+	}
+}
+
