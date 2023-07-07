c943cef347e02ae539b90efe2bd63be7d8ff877f
Mikhail Shashin <shashinma@icloud.com>
2023-07-07 15:19:26 +0300

    Create QnAsController.cs

---
+++ VoteApp/Controllers/QnAsController.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using Microsoft.AspNetCore.Mvc;
+using VoteApp.BLL.Service;
+using VoteApp.ViewModels;
+
+namespace VoteApp.Controllers
+{
+	public class QnAsController : Controller
+	{
+		private readonly IExamService _examService;
+		private readonly IQnAService _qnAService;
+
+        public QnAsController(IExamService examService, IQnAService qnAService)
+        {
+            _examService = examService;
+            _qnAService = qnAService;
+        }
+
+        public IActionResult Index(int pageNumber = 1, int pageSize = 10)
+		{
+			return View(_qnAService.GetAll(pageNumber, pageSize));
+		}
+
+		public IActionResult Create()
+		{
+			var model = new QnAsViewModel();
+			model.ExamList = _examService.GetAllExams();
+
+			return View(model);
+		}
+
+		public async Task<IActionResult> Create(QnAsViewModel qnViewModel)
+		{
+			if (ModelState.IsValid)
+			{
+				await _qnAService.AddAsync(qnViewModel);
+
+				return RedirectToAction(nameof(Index));
+			}
+
+			return View(qnViewModel);
+		}
+	}
+}
+
