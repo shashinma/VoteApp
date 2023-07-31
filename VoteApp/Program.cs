252cf98bfed704cc9b8d85bfe02609247dfdc055
Mikhail Shashin <shashinma@icloud.com>
2023-07-31 20:34:09 +0300

    Update Program.cs

--- VoteApp/Program.cs
+++ VoteApp/Program.cs
@@ -42,7 +42,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Users}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
 
