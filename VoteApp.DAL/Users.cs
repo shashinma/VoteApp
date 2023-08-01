f8be4169bcf8d348bf0650120567fa45714e45f6
Mikhail Shashin <shashinma@icloud.com>
2023-08-01 14:02:57 +0300

    Update Users.cs

--- VoteApp.DAL/Users.cs
+++ VoteApp.DAL/Users.cs
@@ -1,6 +1,18 @@
-﻿
-namespace VoteApp.DAL
+﻿namespace VoteApp.DAL
 {
+
+	/*
+	 * Класс Users содержит поля из таблицы users
+	 * 
+	 * Id - id в базе данных
+	 * Name - имя пользователя на русском
+	 * Username - юзернейм пользователя
+	 * Password - пароль
+	 * Role - роль пользователя
+	 * Groups - группа к которой был отнесен пользователь (категория, отдел)
+	 * 
+	 */
+
 	public class Users
 	{
 		public int Id { get; set; }
