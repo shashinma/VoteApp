33cb7b74c1279bf6b099358159062991f2d9a295
Mikhail Shashin <shashinma@icloud.com>
2023-08-01 14:03:45 +0300

    Update Startup.cs

--- VoteApp/Startup.cs
+++ VoteApp/Startup.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using VoteApp.BLL.Service;
+using VoteApp.DAL.Data;
+using VoteApp.DAL.UnitOfWork;
 
 namespace VoteApp
 {
     public class Startup
     {
-        public void Configure(IApplicationBuilder app)
+        public Startup (IConfiguration configuration)
         {
-            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            services.AddTransient<IGroupService, GroupService>();
+            services.AddTransient<IStudentService, StudentService>();
+            services.AddTransient<IExamService, ExamService>();
+            services.AddTransient<IQnAService, QnAService>();
+            services.AddTransient<IAccountService, AccountService>();
         }
     }
 }
