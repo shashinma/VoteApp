d696be8dac1577154237f40677776b3c70456cdb
Mikhail Shashin <shashinma@icloud.com>
2023-08-01 14:03:30 +0300

    Create UsersController.cs

---
+++ VoteApp/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using VoteApp.BLL.Service;
+using VoteApp.ViewModels;
+
+namespace VoteApp.Controllers
+{ 
+    public class UsersController : Controller 
+    { 
+        private readonly IAccountService _accountService;
+
+        public UsersController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        public IActionResult Index(int pageNumber = 1, int pageSize = 10)
+        {
+            return View(_accountService.GetAllTeachers(pageNumber, pageSize));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(UserViewModel userViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _accountService.AddTeacher(userViewModel);
+                return RedirectToAction("Index");
+            }
+
+            return View(userViewModel);
+        }
+    }
+}
+
